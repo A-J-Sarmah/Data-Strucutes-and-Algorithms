010ba6b0805ac7233baea78beb39953b8c65cbfb
ajsarmah <thisisamlan49@gmail.com>
2021-12-29 17:11:57 +0530

    Added Recursion

---
+++ Algorithms/Recusrsion(Fibonacci Series).py
@@ -0,0 +1,11 @@
+# gets fibonacci number at a certain position.
+
+def get_fib(position):
+    if position == 0 or position==1 : 
+        return position
+    return get_fib(position - 1) + get_fib(position - 2)
+
+
+print(get_fib(9))
+print(get_fib(11))
+print(get_fib(0))
